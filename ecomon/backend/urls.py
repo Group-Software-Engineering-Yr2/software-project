6dc1c49e6acb03904f680b3d950023be1d6cf594
callooomi <callumlincolnward@gmail.com>
2025-02-16 17:03:56 +0000

    added profile path to urls.py and views.py

--- ecomon/backend/urls.py
+++ ecomon/backend/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('example_view',views.example_view),
+    path('profile', views.profile),
 ]
--- ecomon/backend/views.py
+++ ecomon/backend/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here
 
 
 def example_view(request):
     return HttpResponse('Hello, World!')
+
+
+def profile(request):
+    return render(request, 'profile/profile.html')
