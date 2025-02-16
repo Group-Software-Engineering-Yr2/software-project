310af5e965761c4008ff8e590893c16155e3a9aa
OneFishyBoi <ff323@exeter.ac.uk>
2025-02-16 13:00:36 +0000

    added home page and temp for other linked pages

--- ecomon/backend/urls.py
+++ ecomon/backend/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
     path('example_view',views.example_view),
+    path('home', views.home),
+    path('packs', views.packs),
+    path('scanner', views.scanner),
+    path('profile', views.profile),
 ]
--- ecomon/backend/views.py
+++ ecomon/backend/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here
 
-
 def example_view(request):
     return HttpResponse('Hello, World!')
+
+def home(request):
+    return render(request, 'homePage/homePage.html')
+
+def packs(request):
+    return HttpResponse('temp')
+
+def scanner(request):
+    return HttpResponse('temp')
+
+def profile(request):
+    return HttpResponse('temp')
