04409f699e68f385efc03a23fa75898104a52da9
Adam O'Neill <adamo.mark25@gmail.com>
2025-02-16 14:06:03 +0000

    Created root redirect

--- ecomon/backend/views.py
+++ ecomon/backend/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here
 
 
 def example_view(request):
     return HttpResponse('Hello, World!')
+
+def index(request):
+    '''Redirects user based on authentication status'''
+    if request.user.is_authenticated:
+        return redirect('/home')
+    else:
+        return redirect('/accounts/register')
