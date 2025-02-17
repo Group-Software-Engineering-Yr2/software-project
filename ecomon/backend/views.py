634026a66060df149eac90e70f4ed9eceb232fec
Adam O'Neill <adamo.mark25@gmail.com>
2025-02-17 16:20:19 +0000

    Added required login & altered db static upload to

--- ecomon/backend/views.py
+++ ecomon/backend/views.py
@@ -1,20 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here
-
-def example_view(request):
-    return HttpResponse('Hello, World!')
-
+@login_required
 def home(request):
     return render(request, 'homePage/homePage.html')
 
+@login_required
 def packs(request):
     return HttpResponse('temp')
 
+@login_required
 def scanner(request):
     return HttpResponse('temp')
 
+@login_required
 def profile(request):
     return HttpResponse('temp')
 
