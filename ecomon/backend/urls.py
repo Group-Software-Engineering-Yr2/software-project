2da91d97cdc3f18cfa2eeb158867ed5df363a68e
Lorenzo <lm982@exeter.ac.uk>
2025-02-17 15:47:02 +0000

    fixed urls for scanner

--- ecomon/backend/urls.py
+++ ecomon/backend/urls.py
@@ -2,9 +2,9 @@
 URL configuration for ecomon project.
 """
 from django.urls import path
-from .views import render_scanner, render_gym_battle
+from . import views
 
 urlpatterns = [
-    path('scanner/', render_scanner, name='scanner'),
-    path('gym-battle/<str:gym_id>/', render_gym_battle, name='gym-battle'),
+    path('scanner/', views.render_scanner, name='scanner'),
+    path('gym-battle/<str:gym_id>/', views.render_gym_battle, name='gym-battle'),
 ]
