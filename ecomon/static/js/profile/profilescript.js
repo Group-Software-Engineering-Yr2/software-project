2218e1464079e114a5f44650f6a3493f661fa816
Jake-Mc42 <jakemcallister42@gmail.com>
2025-02-18 15:42:21 +0000

    Updated profile html and js

--- ecomon/static/js/profile/profilescript.js
+++ ecomon/static/js/profile/profilescript.js
@@ -1,14 +1,34 @@
 
-/*document.addEventListener("DOMContentLoaded", function () {
-    let cardImage = document.querySelectorAll(".clollected-card");
+document.addEventListener("DOMContentLoaded", function () {
+    let cardImage = document.querySelectorAll(".clollected-cards");
 
-playerCards.forEach(playerCards => {
-    if (use_count > 0){
-        let isUnlocked = card.getAttribute("data-unlocked") === "true";
-        let cardImage = card.getAttribute("data-card-src");
-    }
-    else {
-        let isUnlocked = card.getAttribute("data-unlocked") === "false";
-        let blankImage = card.getAttribute("data-blank-src");
-    }
-});*/
+    playerCards.forEach(playerCards => {
+        if (use_count > 0){
+            let isUnlocked = card.getAttribute("data-unlocked") === "true";
+            let cardImage = card.getAttribute("data-card-src");
+        }
+        else {
+            let isUnlocked = card.getAttribute("data-unlocked") === "false";
+            let blankImage;
+            if (cardType === 0) {
+                blankImage = "/static/images/profile/PlasticBlankBorder.png";
+            } else if (cardType === 1) {
+                blankImage = "/static/images/profile/RecycleBlankBorder.png";
+            } else {
+                blankImage = "/static/images/profile/PlantBlankBorder.png";
+            }
+        
+        }
+
+        card.src = isUnlocked ? cardImage : blankImage;
+
+        // Example: Unlocking on Click (For Testing)
+        card.addEventListener("click", function () {
+            if (!isUnlocked) {
+                isUnlocked = true;
+                card.src = cardImage;
+                card.setAttribute("data-unlocked", "true"); // Update attribute
+            }
+        });
+    });
+});
