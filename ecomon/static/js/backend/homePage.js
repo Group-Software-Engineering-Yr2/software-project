8271489932502781608300cf283431a4cd5b41c9
OneFishyBoi <ff323@exeter.ac.uk>
2025-02-18 15:23:51 +0000

    Added initial map background functionality

---
+++ ecomon/static/js/backend/homePage.js
@@ -0,0 +1,7 @@
+// map functionality
+var map = L.map('map');
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
