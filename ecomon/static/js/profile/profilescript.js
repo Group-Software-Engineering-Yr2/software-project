d8038510e19fee47657ecd1fb49fe5d64397f2b4
Jake-Mc42 <jakemcallister42@gmail.com>
2025-02-18 18:19:25 +0000

    Removed __pycache__ from repository
    Editing profile

--- ecomon/static/js/profile/profilescript.js
+++ ecomon/static/js/profile/profilescript.js
@@ -3,22 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
     let cardImage = document.querySelectorAll(".collected-cards");
 
     playerCards.forEach(playerCards => {
-        if (use_count > 0){
-            let isUnlocked = card.getAttribute("data-unlocked") === "true";
-            let cardImage = card.getAttribute("data-card-src");
+        let cardImage = playerCards.getAttribute("data-card-src");
+        let cardType = playerCards.getAttribute("data-card-type");
+        let useCount = parseInt(card.getAttribute("data-use-count"));
+        let blankImage;
+
+        // Determine which blank border to show based on card type
+        if (cardType === "0") {
+            blankImage = "/static/images/profile/PlasticBlankBorder.png";
+        } else if (cardType === "1") {
+            blankImage = "/static/images/profile/RecycleBlankBorder.png";
+        } else {
+            blankImage = "/static/images/profile/PlantBlankBorder.png";
         }
-        else {
-            let isUnlocked = card.getAttribute("data-unlocked") === "false";
-            let blankImage;
-            if (cardType === 0) {
-                blankImage = "/static/images/profile/PlasticBlankBorder.png";
-            } else if (cardType === 1) {
-                blankImage = "/static/images/profile/RecycleBlankBorder.png";
-            } else {
-                blankImage = "/static/images/profile/PlantBlankBorder.png";
-            }
-        
-        }
+
+        // Show actual card image if use_count > 0, otherwise show blank border
+        card.src = useCount > 0 ? cardImage : blankImage;
 
         card.src = isUnlocked ? cardImage : blankImage;
 
