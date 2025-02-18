2ba2100f77dcb606d7ebc6c71097c4d8922ecf3b
Adam O'Neill <adamo.mark25@gmail.com>
2025-02-18 19:23:39 +0000

    Updated gym admin and start the battle gym endpoint

--- ecomon/backend/admin.py
+++ ecomon/backend/admin.py
@@ -7,8 +7,11 @@ from .models import Team, Card, Gym, PlayerCards, Achievement, PlayerAchievement
 
 admin.site.register(Team)
 admin.site.register(Card)
-admin.site.register(Gym)
 admin.site.register(PlayerAchievements)
 admin.site.register(PlayerCards)
 admin.site.register(Achievement)
 
+@admin.register(Gym)
+class GymAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    list_display = [field.name for field in Gym._meta.fields]
--- ecomon/backend/views.py
+++ ecomon/backend/views.py
@@ -2,20 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .pack_service import get_pack_count, reduce_user_pack_count,generate_pack, add_player_cards
+from .models import Gym
 
 # Create your views here
 
 @login_required
 def home(request):
     return render(request, 'backend/homePage.html')
-
-@login_required
-def packs(request):
-    return render(request, 'packs/packobject.html')
-
-@login_required
-def scanner(request):
-    return HttpResponse('temp')
 
 @login_required
 def profile(request):
@@ -65,4 +58,10 @@ def render_scanner(request):
 
 # @login_required
 def render_gym_battle(request, gym_id):
+    try:
+        gym = Gym.objects.get(id=gym_id)
+    except Gym.DoesNotExist:
+        # Todo update this to custom template
+        return HttpResponse('Gym does not exist')
+    
     return render(request, "backend/gym_battle.html", {"gym_id": gym_id})
