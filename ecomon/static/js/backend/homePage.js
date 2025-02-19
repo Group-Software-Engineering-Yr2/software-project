84c4f2be3db23aa49f99d69132ff8928d9a6639e
Boris Cheung <cactobyte@gmail.com>
2025-02-19 16:18:38 +0000

    Added all Gym Locations into the db

--- ecomon/static/js/backend/homePage.js
+++ ecomon/static/js/backend/homePage.js
@@ -1,5 +1,6 @@
 // map functionality
-var map = L.map('map');
+var map = L.map('map').setView([50.73514962, -3.53428346], 13);
+var testmarker = L.marker([50.73514962, -3.53428346]).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
