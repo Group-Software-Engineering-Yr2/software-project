ea414319d2a3e02ae89f6b354645769dd529524c
callooomi <callumlincolnward@gmail.com>
2025-03-04 03:48:42 +0000

    Added flip card affect to profile cards. Also converted profile and Change deck so they have a corresponding JS file and not have the javascript included

---
+++ technical-documents/source-code/ecomon/static/js/backend/profile/changeDeck.js
@@ -0,0 +1,20 @@
+// Limit the amount of cards a player can select to 3
+function limitSelections(checkbox) {
+    const maxAllowed = 3;
+    let checkboxes = document.getElementsByName('selected_cards');
+    let checked = document.querySelectorAll('input[name="selected_cards"]:checked');
+
+    if (checked.length > maxAllowed) {
+        checkbox.checked = false;
+    }
+}
+
+// Check if the player has selected 3 cards
+function lessThanThree() {
+    let checked = document.querySelectorAll('input[name="selected_cards"]:checked');
+    if (checked.length < 3) {
+        alert("You must select 3 cards to update your deck.");
+        return false;
+    }
+    return true;
+}
