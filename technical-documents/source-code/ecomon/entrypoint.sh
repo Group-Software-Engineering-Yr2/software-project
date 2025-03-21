1e46ba22bb4cfe0b608b7e04227dd00064b83d06
Theocat321 <adamo.mark25@gmail.com>
2025-03-21 15:30:17 +0000

    Added cron tab enabled

--- technical-documents/source-code/ecomon/entrypoint.sh
+++ technical-documents/source-code/ecomon/entrypoint.sh
@@ -2,5 +2,7 @@
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
+python manage.py crontab add
+python manage.py crontab add
 
 gunicorn ecomon.wsgi:application --bind 0.0.0.0:9765
